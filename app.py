a5043877bdcefcf73b827ab1a6dd1eb472a87d73
Eyosi Solo <eyosisolo@gmail.com>
2026-08-06 11:27:40 +0300

    Complete Day 3: User registration with Flask and SQLite

--- app.py
+++ app.py
@@ -1,9 +1,55 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from database import get_connection
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    static_folder="static",
+    template_folder="templates"
+)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+
+        username = request.form["username"]
+        password = request.form["password"]
+
+        connection = get_connection()
+
+        try:
+            cursor = connection.cursor()
+
+            cursor.execute("""
+            CREATE TABLE IF NOT EXISTS users(
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT UNIQUE,
+                password TEXT
+            )
+            """)
+
+            cursor.execute(
+                """
+                INSERT INTO users(username, password)
+                VALUES (?, ?)
+                """,
+                (username, password)
+            )
+
+            connection.commit()
+
+        finally:
+            connection.close()
+
+        return "Registration Successful!"
+
+    return render_template("register.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
